f7db7d2d1137b96cca2c13a9d0cac2cbecbb2cba
mzavattaro <mwzavattaro@icloud.com>
2022-12-21 16:13:30 +1100

    Add authorId to schema

--- src/pages/noticeboard.tsx
+++ src/pages/noticeboard.tsx
@@ -1,10 +1,11 @@
+import { type NextPage } from "next";
 import ToolBar from "../components/ToolBar";
 import ColumnLayout from "../components/ColumnLayout";
 import Header from "../components/Header";
 import StackedList from "../components/StackedList";
 import communityNotices from "../data/communityNotice";
 
-export default function Noticeboard() {
+const Noticeboard: NextPage = () => {
   return (
     <div className="mx-auto max-w-screen-2xl text-gray-900">
       <div className="max-w-screen-2xl px-6">
@@ -16,4 +17,6 @@ export default function Noticeboard() {
       </div>
     </div>
   );
-}
+};
+
+export default Noticeboard;
