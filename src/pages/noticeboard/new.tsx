e57e66e84b187a9d66b6100c318f98dec6d64727
mzavattaro <mwzavattaro@icloud.com>
2022-12-24 11:12:00 +1100

    Add basic react hook form support

--- src/pages/noticeboard/new.tsx
+++ src/pages/noticeboard/new.tsx
@@ -1,42 +1,77 @@
 import { type NextPage } from "next";
-import { useState } from "react";
+// import { useState } from "react";
 import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { trpc } from "../../utils/trpc";
+import type { SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 export const noticeSchema = z.object({
-  title: z
-    .string({
-      required_error: "Title is required",
-    })
-    .min(3),
+  title: z.string().min(1, { message: "Title is required" }),
 });
 
-const New: NextPage = () => {
-  const [title, setTitle] = useState<string>("");
-  const [error, setError] = useState<string | null>(null);
+type NoticeSchema = z.infer<typeof noticeSchema>;
 
+const New: NextPage = () => {
+  // const [title, setTitle] = useState<string>("");
+  // const [error, setError] = useState<string | null>(null);
   const { mutateAsync } = trpc.notice.create.useMutation();
 
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    console.log(title);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<NoticeSchema>({
+    resolver: zodResolver(noticeSchema),
+  });
 
+  const onSubmit: SubmitHandler<NoticeSchema> = async (data) => {
     try {
-      await noticeSchema.parse({ title });
+      await noticeSchema.parse(data);
     } catch (error) {
       if (error instanceof Error) {
-        setError(error.message);
+        console.log(error.message);
       }
       return;
     }
+    mutateAsync(data);
+    console.log(data);
+  };
+  // async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  //   e.preventDefault();
+  //   console.log(title);
 
-    mutateAsync({ title });
-  }
+  //   try {
+  //     await noticeSchema.parse({ title });
+  //   } catch (error) {
+  //     if (error instanceof Error) {
+  //       setError(error.message);
+  //     }
+  //     return;
+  //   }
+  //   console.log({ title });
+  //   mutateAsync({ title });
+  // }
 
   return (
     <div className="mx-auto max-w-screen-2xl text-gray-900">
       <div className="max-w-screen-2xl px-6">
-        {error && JSON.stringify(error)}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            id="title"
+            type="text"
+            {...register("title", { required: true })}
+            placeholder="Title..."
+          />
+          {errors.title && (
+            <p className="mt-2 text-xs italic text-red-500">
+              {" "}
+              {errors.title?.message}
+            </p>
+          )}
+          <button type="submit">submit</button>
+        </form>
+        {/* {error && JSON.stringify(error)}
         <form onSubmit={handleSubmit}>
           <input
             className="bg-gray-200"
@@ -44,7 +79,7 @@ const New: NextPage = () => {
             onChange={(e) => setTitle(e.target.value)}
           />
           <button type="submit">submit</button>
-        </form>
+        </form> */}
       </div>
     </div>
   );
