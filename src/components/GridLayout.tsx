980105b1c9f9b197293434e159848a2aea4a9a93
mzavattaro <mwzavattaro@icloud.com>
2023-01-12 16:47:14 +1100

    Minor styling changes to card

--- src/components/GridLayout.tsx
+++ src/components/GridLayout.tsx
@@ -60,7 +60,7 @@ const GridLayout = () => {
         {notices.map((notice) => (
           <li
             key={notice.id}
-            className="col-span-1 flex flex-col rounded border bg-white"
+            className="col-span-1 flex flex-col rounded border bg-white shadow"
           >
             <div className="-mt-px flex divide-x divide-gray-200 border-b text-center">
               <div className="flex w-0 flex-1">
@@ -92,7 +92,7 @@ const GridLayout = () => {
                 <PdfViewer />
               </div>
               <div className="mt-4 border-t px-4 pb-4">
-                <h3 className="mt-6 text-sm font-medium text-gray-900">
+                <h3 className="mt-3 text-sm font-medium text-gray-900">
                   {notice.title}
                 </h3>
                 <div className="mt-1 flex flex-grow flex-col justify-between">
