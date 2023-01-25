92e248b2a20d75871fb3c93a60e82a60922b75aa
Michael Zavattaro <mwzavattaro@icloud.com>
2023-01-25 13:34:26 +1100

    Add placeholder notice dynamic route page

---
+++ src/pages/noticeboard/notice/[id].tsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+
+const Notice = () => {
+  const router = useRouter();
+  //   const { id } = router.query;
+
+  //   console.log("id", id);
+  return <p>Post: </p>;
+};
+
+export default Notice;
