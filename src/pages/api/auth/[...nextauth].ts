973cf14bd1c3983b5724acc8c6bff8dcaf4a5ea1
Michael Zavattaro <mwzavattaro@icloud.com>
2023-01-31 15:01:24 +1100

    Remove Discord provider and add team page

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,5 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import EmailProvider from "next-auth/providers/email";
-// import nodemailer from "nodemailer";
-import DiscordProvider from "next-auth/providers/discord";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
@@ -32,10 +30,6 @@ export const authOptions: NextAuthOptions = {
       },
       from: "no-reply@getbrisby.com",
       maxAge: 10 * 60, // Magic links are valid for 10 min only
-    }),
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
--- src/pages/signup/team/index.tsx
+++
@@ -1,7 +0,0 @@
-import { type NextPage } from "next";
-
-const Team: NextPage = () => {
-  return <div>Email registration</div>;
-};
-
-export default Team;
