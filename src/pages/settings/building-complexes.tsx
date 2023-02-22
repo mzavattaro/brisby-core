36cceef2564e67d5e0a7d2c91f0665b9d7867c80
Michael Zavattaro <mwzavattaro@icloud.com>
2023-02-22 15:17:11 +1100

    Change name to file name in notices table

--- src/pages/settings/building-complexes.tsx
+++ src/pages/settings/building-complexes.tsx
@@ -107,7 +107,7 @@ const BuildingComplexes: NextPageWithLayout = () => {
                           <td className="px-3 py-4 text-xs text-gray-500">
                             {building.streetAddress}
                           </td>
-                          <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                          <td className="py-4 pl-3 pr-4 text-right text-xs font-medium sm:pr-6">
                             <a
                               href="#"
                               className="text-indigo-600 hover:text-indigo-900"
--- src/pages/settings/notices.tsx
+++ src/pages/settings/notices.tsx
@@ -40,7 +40,7 @@ const Notices: NextPageWithLayout = () => {
                   scope="col"
                   className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
                 >
-                  Name
+                  File name
                 </th>
                 <th
                   scope="col"
