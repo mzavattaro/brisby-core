da43149d74747b060cf85a1d1b1660c2a765dd0e
Michael Zavattaro <mwzavattaro@icloud.com>
2023-02-23 10:47:26 +1100

    Update notice item styling

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -10,13 +10,12 @@ const Tag: FC<Badge> = ({ status, children }) => {
   return (
     <div
       className={classNames(
-        "w-fit rounded-b-sm px-2 py-1 text-xs font-medium",
         status === "draft" &&
-          "inline-flex items-center rounded bg-red-100 px-2 py-0.5 text-xs font-medium capitalize text-red-800",
+          "inline-flex items-center rounded-md bg-red-100 px-2.5 py-0.5 text-sm font-medium capitalize text-red-800",
         status === "published" &&
-          "inline-flex items-center rounded bg-green-100 px-2 py-0.5 text-xs font-medium capitalize text-green-800",
+          "inline-flex items-center rounded-md bg-green-100 px-2.5 py-0.5 text-sm font-medium capitalize text-green-800",
         status === "archived" &&
-          "inline-flex items-center rounded bg-gray-100 px-2 py-0.5 text-xs font-medium capitalize text-gray-800"
+          "inline-flex items-center rounded-md bg-gray-100 px-2.5 py-0.5 text-sm font-medium capitalize text-gray-800"
       )}
     >
       {children}
--- src/components/NoticeItem.tsx
+++ src/components/NoticeItem.tsx
@@ -68,45 +68,33 @@ const NoticeItem: FC<NoticeItem> = ({ notice, toggle }) => {
         className="col-span-1 flex flex-col rounded border bg-white shadow"
       >
         <div className="-mt-px flex divide-x divide-gray-200 border-b text-center">
-          <div className="flex w-0 flex-1">
-            <div className="indivne-flex relative -mr-px w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-2 text-sm font-medium text-gray-700 hover:text-gray-500">
-              <div className="flex flex-col">
-                <span className="text-xs font-bold">Start date</span>
-                <span className="text-xs text-gray-400">
-                  {dayjs(startDate).format("D MMMM YYYY")}
-                </span>
-              </div>
-            </div>
+          <div className="my-2 flex w-full flex-col justify-center">
+            <p className="text-sm font-semibold uppercase">notice period</p>
+            <p className="text-sm text-gray-500">
+              {`${dayjs(startDate).format("D MMMM YYYY")} -
+                  ${dayjs(endDate).format("D MMMM YYYY")}`}
+            </p>
           </div>
-          <div className="-ml-px flex w-0 flex-1">
-            <div className="indivne-flex relative w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent pt-2 text-sm font-medium text-gray-700 hover:text-gray-500">
-              <div className="flex flex-col">
-                <span className="text-xs font-bold">End date</span>
-                <span className="text-xs text-gray-400">
-                  {dayjs(endDate).format("D MMMM YYYY")}
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="mx-auto">
-          <Badge status={status}>{status}</Badge>
         </div>
         <div className="relative flex flex-1 flex-col">
           <div className="aspect-[1/1.414] flex-shrink-0">
             <PdfViewer uploadUrl={uploadUrl} />
           </div>
           <div className="mt-4 border-t px-4 pb-4">
-            <h3 className="mt-3 mb-6 text-sm font-medium text-gray-900 line-clamp-2">
+            <div className="align mt-1 w-full text-center">
+              <Badge status={status}>{status}</Badge>
+            </div>
+
+            <h3 className="mb-2 text-base font-semibold text-gray-900 line-clamp-2">
               {title}
             </h3>
-            <span className="text-sm font-semibold text-gray-900">
-              Uploaded by
-            </span>
-            <div className="flex place-content-between">
-              <span className=" truncate text-sm text-gray-500">
-                {author?.name}
-              </span>
+            <div className="flex place-content-between items-center">
+              <div className="flex flex-col sm:w-40">
+                <span className="text-sm text-gray-900">Uploaded by</span>
+                <span className=" truncate text-sm text-gray-500">
+                  {author?.name}
+                </span>
+              </div>
               <DropdownMenu
                 handleDelete={handleDelete}
                 handlePublishChange={handlePublishChange}
