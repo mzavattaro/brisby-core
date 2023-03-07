a7a3039419cf2eeca1e645f60108a52145551db7
Michael Zavattaro <mwzavattaro@icloud.com>
2023-03-08 10:03:40 +1100

    Add react email and update dependencies

---
+++ react-email-starter/.react-email/.next/server/emails_airbnb_tsx.js
@@ -0,0 +1,24 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "emails_airbnb_tsx";
+exports.ids = ["emails_airbnb_tsx"];
+exports.modules = {
+
+/***/ "./emails/airbnb.tsx":
+/*!***************************!*\
+  !*** ./emails/airbnb.tsx ***!
+  \***************************/
+/***/ (() => {
+
+eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL2VtYWlscy9haXJibmIudHN4LmpzIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./emails/airbnb.tsx\n");
+
+/***/ })
+
+};
+;
