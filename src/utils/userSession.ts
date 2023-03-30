754be53971d5013caf52935618c758f22bb94edd
Michael Zavattaro <mwzavattaro@icloud.com>
2023-03-30 14:29:06 +1100

    Remove userSession utility

--- src/utils/userSession.ts
+++
@@ -1,8 +0,0 @@
-import type { Session } from 'next-auth';
-import { useSession } from 'next-auth/react';
-
-export const UserSession = (): Session['user'] | null => {
-  const { data: sessionData } = useSession();
-
-  return sessionData?.user;
-};
