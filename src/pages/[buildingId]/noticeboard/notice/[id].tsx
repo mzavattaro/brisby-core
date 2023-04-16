2b8b0aa6aa040713895984f33cf8c3278e7ed8ee
Michael Zavattaro <mwzavattaro@icloud.com>
2023-04-17 09:17:46 +1000

    Remove place holder button

--- src/pages/[buildingId]/noticeboard/notice/[id].tsx
+++ src/pages/[buildingId]/noticeboard/notice/[id].tsx
@@ -318,15 +318,9 @@ const Notice = (props: { notice: NoticeByIdOutput }) => {
                   toggleDeleteModal();
                 }}
                 type="button"
-                className="inline-flex items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-100"
+                className="inline-flex items-center justify-center rounded-md border border-red-300 bg-white px-4 py-2 text-sm font-medium text-red-700 shadow-sm hover:bg-red-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-100"
               >
                 Delete
-              </button>
-              <button
-                type="button"
-                className="inline-flex items-center justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-100"
-              >
-                Advance to offer
               </button>
             </div>
           </div>
