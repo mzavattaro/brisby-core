3095a68679788efb430e5dfc6ece0a662af73fd6
Michael Zavattaro <mwzavattaro@icloud.com>
2023-05-22 16:53:51 +1000

    Fix import error

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -7,7 +7,7 @@ import type { SubmitHandler } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import { classNames } from '../../utils/classNames';
-import Email from '../../../public/Email';
+import Email from '../../../icons/Email';
 import type { FC } from 'react';
 import type { GetServerSideProps } from 'next';
 
@@ -91,7 +91,7 @@ const SignIn: FC<SignInSchema> = ({ csrfToken }) => {
           <button
             disabled={isSubmitting}
             className={classNames(
-              'mt-10 flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2',
+              'mt-10 flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2',
               isSubmitting && 'cursor-not-allowed opacity-50'
             )}
             type="submit"
